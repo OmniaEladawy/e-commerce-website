3365f59d443d0acd9708d0daaad89693183f3b23
Omnia Eladawy <omnia.osama@intellaworld.com>
2023-05-08 15:31:12 +0300

    add filter and pagination functions

--- backend/utils/ApiFilters.js
+++ backend/utils/ApiFilters.js
@@ -20,10 +20,39 @@ class APIFilters {
 
 	filter() {
 		const queryCopy = { ...this.queryStr };
+
 		const removeFields = ['keyword', 'page'];
 		removeFields.forEach((el) => delete queryCopy[el]);
 
-		this.query = this.query.find(queryCopy);
+		let output = {};
+		let prop = '';
+
+		for (let key in queryCopy) {
+			if (!key.match(/\b(gt|gte|lt|lte)/)) {
+				output[key] = queryCopy[key];
+			} else {
+				prop = key.split('[')[0];
+
+				let operator = key.match(/\[(.*)\]/)[1];
+
+				if (!output[prop]) {
+					output[prop] = {};
+				}
+
+				output[prop][`$${operator}`] = queryCopy[key];
+			}
+		}
+		// { price: { $gte: 100, $lte: 1000 } }
+
+		this.query = this.query.find(output);
+		return this;
+	}
+
+	pagination(resPerPage) {
+		const currentPage = Number(this.queryStr.page) || 1;
+		const skip = resPerPage * (currentPage - 1);
+
+		this.query = this.query.limit(resPerPage).skip(skip);
 		return this;
 	}
 }
