fad4b07595d7e4d2e706d09eb07e9937a388d4cc
oeladawy <omnia1699@gmail.com>
2023-05-13 18:26:36 +0300

    handle category and ratings filter

--- app/page.js
+++ app/page.js
@@ -10,6 +10,8 @@ const getProducts = async (searchParams) => {
 	const urlParams = {
 		keyword: searchParams.keyword,
 		page: searchParams.page,
+		category: searchParams.category,
+		'ratings[gte]': searchParams.ratings,
 	};
 
 	const searchQuery = queryString.stringify(urlParams);
